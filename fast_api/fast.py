1144e0501036e167b378cb03c38828d9f523e52e
Joe Vincent <hellojustjoe@gmail.com>
2024-02-12 17:00:49 -0600

    pushing api changes, api is working when hosted locally. Need to get the google cloud storage fixed and then host it.

--- fast_api/fast.py
+++ fast_api/fast.py
@@ -12,20 +12,16 @@ import plotly.express as px
 
 app = FastAPI()
 
-from fastapi import FastAPI
-
-app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-
 @app.get('/name-alcaldia')
 def get_name_alcaldia():
 
-    with open('data/latlon.json', 'r') as archivo:
+    with open('../data/latlon.json', 'r') as archivo:
         data = json.load(archivo)
 
     names_alcaldias = data['Alcaldia']
@@ -35,7 +31,7 @@ def get_name_alcaldia():
 @app.get('/latlon')
 def get_latlon(name_alcaldia: str):
 
-    with open('data/latlon.json', 'r') as archivo:
+    with open('../data/latlon.json', 'r') as archivo:
         data = json.load(archivo)
 
     indice_alcaldia = data['Alcaldia'].index(name_alcaldia)
@@ -48,7 +44,7 @@ def get_latlon(name_alcaldia: str):
 
 @app.get('/main-map')
 def get_main_map():
-    mapa = gpd.read_file('data/joe_map.geojson')
+    mapa = gpd.read_file('../data/joe_map.geojson')
     mapa = json.loads(mapa.to_json())
     return mapa
 
@@ -56,7 +52,7 @@ def get_main_map():
 def get_dynamic_data(name_alcaldia: str):
 
     # Verificar si el archivo ya existe localmente
-    local_file_path = f'data/{name_alcaldia}_data.csv'
+    local_file_path = f'../data/{name_alcaldia}_data.csv'
 
     if os.path.exists(local_file_path):
         data_alcaldia = pd.read_csv(local_file_path)
@@ -103,32 +99,36 @@ def get_dynamic_data(name_alcaldia: str):
 
 @app.get('/model-data')
 def get_model_data(name_alcaldia: str):
-    # Verificar si el archivo ya existe localmente
-    local_file_path = f'data/predictions/{name_alcaldia}_pred.csv'
-    if os.path.exists(local_file_path):
-        pred_alcaldia = pd.read_csv(local_file_path)
-    else:
-        #gc info
-        name_alcaldia = name_alcaldia.replace(' ', '_')
-        bucket_name = os.getenv('BUCKET_NAME')
-        file_name = f'predictions/{name_alcaldia}_pred.csv'
-        project_id = os.getenv('GCP_PROJECT')
-        # Configura la conexión a gc
-        client = storage.Client(project=project_id)
-        bucket = client.bucket(bucket_name)
-        # Descarga el archivo CSV como BytesIO
-        blob = bucket.blob(file_name)
-        content = blob.download_as_text()
-        csv_data = BytesIO(content.encode('utf-8'))
-        pred_alcaldia = pd.read_csv(csv_data)
-        # Convierte la columna 'fecha_creacion' a tipo datetime
-        pred_alcaldia['semana_creacion'] = pd.to_datetime(pred_alcaldia['semana_creacion'])
-        # Extrae el año y el mes de la fecha de creación
-        pred_alcaldia['month'] = pred_alcaldia['semana_creacion'].dt.month_name()
-        # Guardar data local
-        pred_alcaldia.to_csv(local_file_path, index=False)
-    # Agrupa por año, mes y tipo de incidente y cuenta la frecuencia
-    data_grouped = pred_alcaldia.groupby('month')['numero_incidentes'].sum()
-    meses = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    result = data_grouped.loc[meses].to_dict()
-    return {'data': result}
+    try:
+        # Verificar si el archivo ya existe localmente
+        local_file_path = f'../data/predictions/{name_alcaldia}_pred.csv'
+        if os.path.exists(local_file_path):
+            pred_alcaldia = pd.read_csv(local_file_path)
+        else:
+            #gc info
+            name_alcaldia = name_alcaldia.replace(' ', '_')
+            bucket_name = os.getenv('BUCKET_NAME')
+            file_name = f'predictions/{name_alcaldia}_pred.csv'
+            project_id = os.getenv('GCP_PROJECT')
+            # Configura la conexión a gc
+            client = storage.Client(project=project_id)
+            bucket = client.bucket(bucket_name)
+            # Descarga el archivo CSV como BytesIO
+            blob = bucket.blob(file_name)
+            content = blob.download_as_text()
+            csv_data = BytesIO(content.encode('utf-8'))
+            pred_alcaldia = pd.read_csv(csv_data)
+            # Convierte la columna 'fecha_creacion' a tipo datetime
+            pred_alcaldia['semana_creacion'] = pd.to_datetime(pred_alcaldia['semana_creacion'])
+            # Extrae el año y el mes de la fecha de creación
+            pred_alcaldia['month'] = pred_alcaldia['semana_creacion'].dt.month_name()
+            # Guardar data local
+            pred_alcaldia.to_csv(local_file_path, index=False)
+        # Agrupa por año, mes y tipo de incidente y cuenta la frecuencia
+        data_grouped = pred_alcaldia.groupby('month')['numero_incidentes'].sum()
+        meses = ["January", "February", "March", "April", "May", "June", "July", "August"]
+        result = data_grouped.loc[meses].to_dict()
+        return {'data': result}
+    except Exception as e:
+        # Log the exception e or print it to the console
+        raise HTTPException(status_code=500, detail=str(e))
